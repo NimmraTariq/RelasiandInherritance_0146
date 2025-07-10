6eeb3102e28023b969c73c28cc335572e59a2d0b
NimmraTariq <nimra369tariq@gmail.com>
2025-07-10 19:28:28 +0700

    Constructor initializes 'nama' using initialization list

---
+++ agregasi/anak.h
@@ -0,0 +1,14 @@
+#ifndef ANAK_H
+#define ANAK_H // Prevent multiple inclusions of the header file
+
+class anak 
+{
+public :
+    string nama ; // Declare public member variable to store the child's name
+
+    anak(string pNama) : nama(pNama) // Constructor initializes 'nama' using initialization list
+    {
+        cout << "Anak\"" << nama << "\" ada \n"; // Print message when anak object is created
+    }
+
+    ~anak()
