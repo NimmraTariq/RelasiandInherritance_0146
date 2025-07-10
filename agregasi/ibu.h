f6ff9e0c4eeb1a3f22b7b23aadd27e27450b028e
NimmraTariq <nimra369tariq@gmail.com>
2025-07-10 19:31:04 +0700

    Store pointers to anak objects (children) in a vector

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -5,3 +5,21 @@ class ibu
 {
 public :
     string nama ;
+     vector <anak *> daftar_anak; // Store pointers to anak objects (children) in a vector
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada \n";
+    }
+
+    ~ibu()
+    {
+         cout << "ibu  \" " << nama << "\"  tida ada \n";
+    }
+
+    void tambahAnak(anak *);
+    void cetakAnak();
+};
+
+void ibu::tambahAnak(anak *pAnak)
+{
