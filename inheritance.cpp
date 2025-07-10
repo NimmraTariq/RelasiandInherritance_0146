9989b5d8553fe71ced174dc9876e82bb9d6dfc5d
NimmraTariq <nimra369tariq@gmail.com>
2025-07-10 19:57:08 +0700

    Create pelajar object with name and school

--- inheritance.cpp
+++ inheritance.cpp
@@ -35,3 +35,9 @@ public:
 };
 
 int main() {
+     pelajar siswa1("andi laksono", "SMAN 1 Bantul"); // Create pelajar object with name and school
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
